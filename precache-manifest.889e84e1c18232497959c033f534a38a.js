d19c6f65cbfd8be6a4b8c1dd6c389a9bff777f4c
bellissima0419 <myrealgeorgia@gmail.com>
2020-04-28 14:58:45 -0400

    Updates

---
+++ precache-manifest.889e84e1c18232497959c033f534a38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7a6b83dee39579f2c1499d00a90f44",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "4b9e88be767a0f7d74f4",
+    "url": "/react-cards/static/css/main.55369abb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-cards/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cards/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9e88be767a0f7d74f4",
+    "url": "/react-cards/static/js/main.7ac4681d.chunk.js"
+  },
+  {
+    "revision": "97d1d0b082fccc30fa81",
+    "url": "/react-cards/static/js/runtime-main.653bfb73.js"
+  }
+]);
